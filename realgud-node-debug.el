ceb6d0f8cee4e884d493cf094a4b1164f898208c
rocky <rb@dustyfeet.com>
2019-05-21 16:43:08 -0400

    email of author

--- realgud-node-debug.el
+++ realgud-node-debug.el
@@ -1,6 +1,6 @@
 ;;; realgud-node-debug.el --- Realgud front-end to older nodejs "node debug" -*- lexical-binding: t -*-
 
-;; Author: Rocky Bernstein
+;; Author: Rocky Bernstein <rocky@gnu.org>
 ;; Version: 1.0.0
 ;; Package-Type: multi
 ;; Package-Requires: ((realgud "1.4.5") (load-relative "1.2") (cl-lib "0.5") (emacs "24"))
